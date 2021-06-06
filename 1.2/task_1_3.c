3fc2bf198103ec611b95e7e2d2b99bb0a5dffc5f
pcade <pahomovgrigorii@gmail.com>
2021-06-06 22:09:07 +0300

    task_1_3.c

---
+++ 1.2/task_1_3.c
@@ -0,0 +1,22 @@
+// таблица вывода температур по Фаренгейту и Цельсию
+
+#include <stdio.h>
+
+void main()
+{
+	int	fahr, celsius;
+	int	lower, upper, step;
+
+	lower = 0;    //  нижняя граница температуры
+	upper = 300;  //  верхняя граница температуры 
+	step = 20;    //  размер шага
+
+	printf("fahr\tcelsius\n");
+	fahr = lower;
+	while (fahr <= upper)
+	{
+		celsius = 5* (fahr -32)/9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}	 
+}
